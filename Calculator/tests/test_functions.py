665dee60c574580b52ecef66d88a0cc83e3dbd2c
Batyrkhan <150823724+batyrkhan4@users.noreply.github.com>
2026-05-20 09:21:22 +0500

    5

--- Calculator/tests/test_functions.py
+++ Calculator/tests/test_functions.py
@@ -1,23 +1,56 @@
 import unittest
-from core.functions import add, subtract, multiply, divide, square
+from core.functions import (
+    square,
+    cube,
+    inverse,
+    square_root,
+    cube_root,
+    absolute,
+    ten_power,
+    two_power,
+    natural_log,
+    common_log,
+    exponent,
+    factorial_num,
+    sine,
+    cosine
+)
 
 
 class TestCalculatorFunctions(unittest.TestCase):
 
-    def test_add(self):
-        self.assertEqual(add(2, 3), 5)
-
-    def test_subtract(self):
-        self.assertEqual(subtract(5, 2), 3)
-
-    def test_multiply(self):
-        self.assertEqual(multiply(4, 3), 12)
-
-    def test_divide(self):
-        self.assertEqual(divide(10, 2), 5)
-
     def test_square(self):
         self.assertEqual(square(4), 16)
+
+    def test_cube(self):
+        self.assertEqual(cube(3), 27)
+
+    def test_inverse(self):
+        self.assertEqual(inverse(2), 0.5)
+
+    def test_square_root(self):
+        self.assertEqual(square_root(25), 5)
+
+    def test_cube_root(self):
+        self.assertAlmostEqual(cube_root(27), 3)
+
+    def test_absolute(self):
+        self.assertEqual(absolute(-10), 10)
+
+    def test_ten_power(self):
+        self.assertEqual(ten_power(2), 100)
+
+    def test_two_power(self):
+        self.assertEqual(two_power(3), 8)
+
+    def test_factorial_num(self):
+        self.assertEqual(factorial_num(5), 120)
+
+    def test_sine(self):
+        self.assertAlmostEqual(sine(90), 1)
+
+    def test_cosine(self):
+        self.assertAlmostEqual(cosine(0), 1)
 
 
 if __name__ == "__main__":
